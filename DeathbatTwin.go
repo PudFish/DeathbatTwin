bbca0e56e42c033958142fe9144019ad40b08ac7
Daniel <chook.myers@gmail.com>
2022-01-15 23:39:46 +1100

    update filename

--- DeathbatTwin.go
+++ DeathbatTwin.go
@@ -67,7 +67,7 @@ var Deathbats []Deathbat
 
 //main handles the high level function calls for now
 func main() {
-	filename := "deathbats1-3000.json"
+	filename := "deathbats1-9000.json"
 	if err := loadDeathbats(filename); err != nil {
 		fmt.Printf("err: main: %s", err)
 		return
